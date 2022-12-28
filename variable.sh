91358c4e9e5cff89a742adc4cfe752b535950521
mundrika <mundrika.raj@gmail.com>
2022-12-28 06:51:33 +0530

    added variable file

---
+++ variable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Variables are areas of memory that can be used to
+# store information and are referred to by a name.
+# In this script,  we created a variable 
+# called "greeting" and placed the value "Good Morning" 
+# into memory
+# variable name and variable value
+# should be separated with equal sign(=).No space before or after
+# equal sign example: greeting="Good Morning"
+
+greeting="Good Morning"
+
+# variable value can be accessed anywhere in the script
+# with its name followed by $ sign. example $greeting
+
+echo $greeting
